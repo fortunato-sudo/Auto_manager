43ffdbddad4583ff173243e70604762eadfbf827
fortunato-sudo <fortunatosamuel.grillo@gmail.com>
2026-03-31 13:27:44 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,37 @@
-const CACHE = "garage-manager-v2";
+const CACHE_NAME = "garage-manager-v2";
 
-const ASSETS = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/js/app.js",
-  "/js/home.js",
-  "/js/manut.js",
-  "/js/fuel.js",
-  "/js/stats.js",
-  "/js/utils.js",
-  "/img/logo.png"
-];
+self.addEventListener("install", event=>{
+  self.skipWaiting();
+});
 
-self.addEventListener("install",e=>{
-  e.waitUntil(
-    caches.open(CACHE)
-    .then(cache=>cache.addAll(ASSETS))
+self.addEventListener("activate", event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>{
+      return Promise.all(
+        keys.map(key=>{
+          if(key !== CACHE_NAME){
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
 });
 
-self.addEventListener("fetch",e=>{
-  e.respondWith(
-    caches.match(e.request)
-    .then(res=>res || fetch(e.request))
+self.addEventListener("fetch", event=>{
+  if(event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response=>{
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache=>{
+          cache.put(event.request, clone);
+        });
+        return response;
+      })
+      .catch(()=>{
+        return caches.match(event.request);
+      })
   );
 });
