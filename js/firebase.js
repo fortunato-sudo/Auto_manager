77f41047844a3c9faf1268710eb4a1ad04247e41
fortunato-sudo <fortunatosamuel.grillo@gmail.com>
2026-03-31 13:23:38 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
 import { 
-	getFirestore,
 	initializeFirestore,
 	persistentLocalCache,
 	collection,
@@ -12,15 +12,14 @@ import {
 	deleteDoc
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const firebaseConfig={
-    apiKey:"AIzaSy...",
-    authDomain:"auto-manutenzione.firebaseapp.com",
-    projectId:"auto-manutenzione"
+const firebaseConfig = {
+	apiKey:"AIzaSy...",
+	authDomain:"auto-manutenzione.firebaseapp.com",
+	projectId:"auto-manutenzione"
 };
 
 const app = initializeApp(firebaseConfig);
 
-/* Firestore molto più veloce su mobile */
 const db = initializeFirestore(app,{
 	localCache: persistentLocalCache()
 });
