f7e0d900994ee7dfc56827910c81c491d8fb0f3f
fortunato-sudo <fortunatosamuel.grillo@gmail.com>
2026-03-31 13:04:48 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,28 @@
-const CACHE_NAME = "garage-manager-v1";
+const CACHE = "garage-manager-v2";
 
-const urlsToCache = [
+const ASSETS = [
   "/",
   "/index.html",
-  "/app.js",
-  "/style.css"
+  "/style.css",
+  "/js/app.js",
+  "/js/home.js",
+  "/js/manut.js",
+  "/js/fuel.js",
+  "/js/stats.js",
+  "/js/utils.js",
+  "/img/logo.png"
 ];
 
-self.addEventListener("install", event=>{
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(cache=>{
-      return cache.addAll(urlsToCache);
-    })
+self.addEventListener("install",e=>{
+  e.waitUntil(
+    caches.open(CACHE)
+    .then(cache=>cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener("fetch", event=>{
-  event.respondWith(
-    caches.match(event.request)
-    .then(response=>{
-      return response || fetch(event.request);
-    })
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request)
+    .then(res=>res || fetch(e.request))
   );
 });
