fc1026bce7d6ee3944dbd9dc7df456b709d1124f
fortunato-sudo <fortunatosamuel.grillo@gmail.com>
2026-03-31 11:17:21 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,11 @@
+import { db, doc, setDoc } from "./firebase.js";
+import { setCacheConfig } from "./state.js";
+
 window.saveKm=async function(){
     let km=document.getElementById("km").value;
     await setDoc(doc(db,"config","auto"),{
         km_attuali:Number(km)
     });
-    cacheConfig = Number(km);
+    setCacheConfig(Number(km));
     render();
 }
