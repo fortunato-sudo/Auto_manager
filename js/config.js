7115262d234fd0cd9429163d47836a62342b8b91
fortunato-sudo <fortunatosamuel.grillo@gmail.com>
2026-04-01 12:53:29 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ import { setCacheConfig } from "./state.js";
 
 window.saveKm=async function(){
     let km=document.getElementById("km").value;
-    await setDoc(doc(db,"config","auto"),{
+    await setDoc(doc(db,"vehicles",vehicleId,"config","auto"),{
         km_attuali:Number(km)
     });
     setCacheConfig(Number(km));
