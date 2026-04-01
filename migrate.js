a3bcdc083e41b57800e807fbbffc6d33d2dc2bcf
fortunato-sudo <fortunatosamuel.grillo@gmail.com>
2026-04-01 12:32:03 +0200

    Update migrate.js

--- migrate.js
+++ migrate.js
@@ -1,45 +1,31 @@
-import { db, collection, getDocs, addDoc } from "./firebase.js";
+import { db, collection, getDocs, doc, setDoc } from "./firebase.js";
 
-async function migrateCollection(oldName,newPath){
+async function migrateCollection(oldName, newPath){
 
-const snap = await getDocs(collection(db,oldName));
+    const snap = await getDocs(collection(db, oldName));
 
-for(const docSnap of snap.docs){
+    for(const d of snap.docs){
 
-await addDoc(
-collection(db,...newPath),
-docSnap.data()
-);
+        const data = d.data();
 
-}
+        await setDoc(
+            doc(db, "vehicles", "qashqai", newPath, d.id),
+            data
+        );
 
-console.log(oldName+" migrata");
+    }
 
+    console.log(oldName + " migrata");
 }
 
 async function startMigration(){
 
-await migrateCollection(
-"fuel",
-["vehicles","qashqai","fuel"]
-);
+    await migrateCollection("fuel", "fuel");
+    await migrateCollection("registro", "registro");
+    await migrateCollection("manutenzioni", "manutenzioni");
+    await migrateCollection("config", "config");
 
-await migrateCollection(
-"registro",
-["vehicles","qashqai","registro"]
-);
-
-await migrateCollection(
-"manutenzioni",
-["vehicles","qashqai","manutenzioni"]
-);
-
-await migrateCollection(
-"config",
-["vehicles","qashqai","config"]
-);
-
-console.log("Migrazione completata");
+    console.log("Migrazione completata");
 
 }
 
